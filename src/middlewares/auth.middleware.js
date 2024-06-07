ddea36fa567d8f36d57d6fe09767528c5deff351
Samuel-for-github <134806257+Samuel-for-github@users.noreply.github.com>
2024-06-08 04:04:57 +0530

    Login Controller.

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -7,7 +7,7 @@ export  const verifyJWT = asyncHandler(async(req, _, next) => {
   try{
     const token =  req.cookies?.accessToken || req.header('Authorization')?.replace('Bearer ', '')
     if (!token) {
-      throw new ApiError(401, "Unauthorized" , 'No token provided');
+       new ApiError(401, "Unauthorized" , 'No token provided');
     }
     const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
     const user = await User.findById(decodedToken?._id).select("-password -refreshToken");
