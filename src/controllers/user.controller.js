1c6d4c1ccfed25e938b5a98e1b7f646b13b41dfd
Samuel-for-github <134806257+Samuel-for-github@users.noreply.github.com>
2024-06-09 01:05:40 +0530

    subscription model

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,7 +4,7 @@ import {User} from "../models/user.model.js";
 import {uploadOnCloudinary} from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken";
-import userRoute from "../routes/user.route.js";
+
 
 const generateAccessAndRefreshTokens = async(userID)=>{
   try{
